b58ecfc05bbf5ded93732ee4420272ad79960394
Benjamin Riviere <benjamin.riviere@linkfluence.com>
2020-03-29 15:03:53 +0200

    Add icons display.

--- assets/js/kiosk.js
+++ assets/js/kiosk.js
@@ -37,6 +37,45 @@ const helpers = {
         setTimeout(() => {
             el.className = "fade-out";
         }, duration);
+    },
+    showIcon(action, duration = 1000) {
+        const el = document.querySelector("#action-icon");
+        el.className = "fade-in";
+
+        const icon = el.querySelector(".material-icons");
+
+        switch(action) {
+            case "play": {
+                icon.textContent = "play_arrow";
+                break;
+            }
+            case "pause": {
+                icon.textContent = "pause";
+                break;
+            }
+            case "mute": {
+                icon.textContent = "volume_mute";
+                break;
+            }
+            case "unmute": {
+                icon.textContent = "volume_up";
+                break;
+            }
+            case "forward": {
+                icon.textContent = "forward_5";
+                // icon.textContent = "fast_forward";
+                break;
+            }
+            case "rewind": {
+                icon.textContent = "replay_5";
+                // icon.textContent = "fast_rewind";
+                break;
+            }
+        }
+
+        setTimeout(() => {
+            el.className = "fade-out";
+        }, duration);
     }
 };
 
