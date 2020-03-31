8cf22c9e647822ebf2c68e0fe4395b95e5132481
Benjamin Riviere <benjamin.riviere@linkfluence.com>
2020-03-31 21:24:40 +0200

    Add settings interface.

---
+++ assets/js/config.js
@@ -0,0 +1,77 @@
+const config = {
+    SCREEN_ID: "default",
+    SERVER_HOST: "localhost",
+    SERVER_PORT: 5000,
+    VERSION: "3.0.0",
+    actualScreenID: null,
+    load() {
+        this.SCREEN_ID = window.localStorage.getItem("screen_id");
+        this.SERVER_HOST = window.localStorage.getItem("server_host");
+        this.SERVER_PORT = parseInt(window.localStorage.getItem("server_port"));
+    },
+    get hasBeenConfigured() {
+        const screenID = window.localStorage.getItem("screen_id");
+        const host = window.localStorage.getItem("server_host");
+        const port = window.localStorage.getItem("server_port");
+
+        return screenID !== null && host !== null && port !== null;
+    },
+    configure(screenID, serverHost, serverPort) {
+        this.SCREEN_ID = screenID || "default";
+        this.SERVER_HOST = serverHost || "localhost";
+        this.SERVER_PORT = parseInt(serverPort) || 5000;
+
+        window.localStorage.setItem("screen_id", this.SCREEN_ID);
+        window.localStorage.setItem("server_host", this.SERVER_HOST);
+        window.localStorage.setItem("server_port", this.SERVER_PORT);
+    },
+    clear() {
+        window.localStorage.clear();
+        window.location.reload();
+    },
+    get serverURL() {
+        return `http://${this.SERVER_HOST}:${this.SERVER_PORT}`;
+    },
+    showPanel() {
+        // Pre-fill some parameters
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = urlParams.get("id");
+
+        if (id) {
+            document.querySelector("#tv-id").value = id;
+        }
+
+        document.querySelector("#settings").style.display = "block";
+    },
+    hidePanel() {
+        document.querySelector("#settings").style.display = "none";
+    },
+    get screenID() {
+        // Make the ID query parameter to have the priority over the config
+        // Therefore, we could have several screens on the same machine
+        // Caching to avoid parsing each time
+        if (!this.actualScreenID) {
+            const urlParams = new URLSearchParams(window.location.search);
+            const id = urlParams.get("id");
+
+            if (id) {
+                this.actualScreenID = id;
+            } else {
+                this.actualScreenID = this.SCREEN_ID;
+            }
+        }
+
+        return this.actualScreenID;
+    }
+};
+
+document.querySelector("#ok-button").addEventListener("click", () => {
+    const id = document.querySelector("#tv-id").value;
+    const host = document.querySelector("#server-host").value;
+    const port = document.querySelector("#server-port").value;
+
+    config.configure(id, host, port);
+
+    // Finally, reload the page
+    window.location.reload();
+});
--- assets/js/kiosk.js
+++ assets/js/kiosk.js
@@ -1,82 +1,86 @@
 // TODO : Rewrite this with TypeScript ?
 // TODO : Perhaps this should be bundled as a WPA
-const SERVER_URL = `http://${config.SERVER_HOST}:${config.SERVER_PORT}`;
-const socket = io(SERVER_URL);
+if (!config.hasBeenConfigured) {
+    // Show the settings panel
+    config.showPanel();
+} else {
+    config.load(); // Load the config from localStorage
+    config.hidePanel();
 
-const iframe = document.querySelector("iframe#player");
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id") || "default";
+    const socket = io(config.serverURL);
+    const iframe = document.querySelector("iframe#player");
 
-helpers.setID(id);
-helpers.setWindowTitle(id, "Waiting for connection...");
+    helpers.setID(config.screenID);
+    helpers.setWindowTitle(config.screenID, "Waiting for connection...");
 
-socket.on(BuiltInEvents.CONNECT, () => {
-    socket.emit(KioskEvents.REGISTER, {
-        id: id,
-        screenSize: `${window.innerWidth}x${window.innerHeight}`,
-        machine: window.navigator.userAgent,
-        version: config.VERSION
+    socket.on(BuiltInEvents.CONNECT, () => {
+        socket.emit(KioskEvents.REGISTER, {
+            id: config.screenID,
+            screenSize: `${window.innerWidth}x${window.innerHeight}`,
+            machine: window.navigator.userAgent,
+            version: config.VERSION
+        });
     });
-});
 
-socket.on(KioskEvents.EXCEPTION, (err) => {
-    iframe.src = `${SERVER_URL}/contents/error/${err.code}`;
+    socket.on(KioskEvents.EXCEPTION, (err) => {
+        iframe.src = `${config.serverURL}/contents/error/${err.code}`;
 
-    if (err.message) {
-        iframe.src += `?details=${encodeURIComponent(err.message)}`;
-    }
+        if (err.message) {
+            iframe.src += `?details=${encodeURIComponent(err.message)}`;
+        }
 
-    helpers.setWindowTitle(id, err.reason);
-});
+        helpers.setWindowTitle(config.screenID, err.reason);
+    });
 
-socket.on(KioskEvents.INIT, (payload) => {
-    iframe.src = payload.content.uri;
+    socket.on(KioskEvents.INIT, (payload) => {
+        iframe.src = payload.content.uri;
 
-    helpers.setWindowTitle(id);
-    helpers.setTitle(payload.content.displayName);
-    helpers.setBrightness(payload.tv.brightness);
-});
+        helpers.setWindowTitle(config.screenID);
+        helpers.setTitle(payload.content.displayName);
+        helpers.setBrightness(payload.tv.brightness);
+    });
 
-socket.on(KioskEvents.DISPLAY, (payload) => {
-    iframe.src = payload.content.uri;
+    socket.on(KioskEvents.DISPLAY, (payload) => {
+        iframe.src = payload.content.uri;
 
-    helpers.setWindowTitle(id);
-    helpers.setTitle(payload.content.displayName);
-});
+        helpers.setWindowTitle(config.screenID);
+        helpers.setTitle(payload.content.displayName);
+    });
 
-socket.on(BuiltInEvents.CONNECT_ERROR, () => {
-    const ERROR_CONTENT = "contents/error.html";
+    socket.on(BuiltInEvents.CONNECT_ERROR, () => {
+        const ERROR_CONTENT = "contents/error.html";
 
-    // This event occurs multiple times
-    if (!iframe.src.endsWith(ERROR_CONTENT)) {
-        iframe.src = ERROR_CONTENT;
-        helpers.setWindowTitle(id, "Waiting for connection...");
-    }
+        // This event occurs multiple times
+        if (!iframe.src.endsWith(ERROR_CONTENT)) {
+            iframe.src = ERROR_CONTENT;
+            helpers.setWindowTitle(config.screenID, "Waiting for connection...");
+        }
 
-    helpers.setBrightness(1);
-});
+        helpers.setBrightness(1);
+    });
 
-socket.on(KioskEvents.IDENTIFY, (payload) => {
-    helpers.showID(payload.duration);
-});
+    socket.on(KioskEvents.IDENTIFY, (payload) => {
+        helpers.showID(payload.duration);
+    });
 
-socket.on(KioskEvents.RELOAD, () => {
-    iframe.src = iframe.src;
-});
+    socket.on(KioskEvents.RELOAD, () => {
+        iframe.src = iframe.src;
+    });
 
-socket.on(KioskEvents.BRIGHTNESS, (payload) => {
-    if (payload.brightness > 0.66) {
-        helpers.showIcon("high_brightness");
-    } else if (payload.brightness < 0.33) {
-        helpers.showIcon("low_brightness");
-    } else {
-        helpers.showIcon("medium_brightness");
-    }
+    socket.on(KioskEvents.BRIGHTNESS, (payload) => {
+        if (payload.brightness > 0.66) {
+            helpers.showIcon("high_brightness");
+        } else if (payload.brightness < 0.33) {
+            helpers.showIcon("low_brightness");
+        } else {
+            helpers.showIcon("medium_brightness");
+        }
 
-    helpers.setBrightness(payload.brightness);
-});
+        helpers.setBrightness(payload.brightness);
+    });
 
-socket.on(KioskEvents.TOGGLE_MUTE, (payload) => {
-    helpers.showIcon(payload.muted ? "mute" : "unmute");
-    helpers.toggleMute(payload.muted, payload.type);
-});
+    socket.on(KioskEvents.TOGGLE_MUTE, (payload) => {
+        helpers.showIcon(payload.muted ? "mute" : "unmute");
+        helpers.toggleMute(payload.muted, payload.type);
+    });
+}
